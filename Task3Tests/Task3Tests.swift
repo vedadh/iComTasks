161653a1fda20737281193daf1cbee71836bc30c
Vedad Hasanagic <vedad.hasanagic@personal.com>
2016-11-12 16:55:56 +0100

    Initial Commit

---
+++ Task3Tests/Task3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Task3Tests.swift
+//  Task3Tests
+//
+//  Created by Vedad Hasanagic on 11/12/16.
+//  Copyright © 2016 Vedad H. All rights reserved.
+//
+
+import XCTest
+@testable import Task3
+
+class Task3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
